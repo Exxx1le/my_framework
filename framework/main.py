ceef4fd6ec5010d1777f6c5fabe50c90c19363ef
Ex1le <3x1le@rambler.ru>
2023-07-20 19:19:17 +0300

    post-requests added

--- framework/main.py
+++ framework/main.py
@@ -1,27 +1,54 @@
+from quopri import decodestring
+from .requests_handler import GetRequests, PostRequests
+
+
+class PageNotFound:
+    def __call__(self, request):
+        return "404", "404 PAGE Not Found"
+
+
 class MyFramework:
-    def __init__(self, routes_obj, fronts_obj):
+    def __init__(self, routes_obj, fronts_obj) -> bytes:
         self.routes_lst = routes_obj
         self.fronts_lst = fronts_obj
 
     def __call__(self, environ, start_response):
-        path = environ['PATH_INFO']
+        path = environ["PATH_INFO"]
 
-        if not path.endswith('/'):
-            path = f'{path}/'
+        if not path.endswith("/"):
+            path = f"{path}/"
+
+        request = {}
+        method = environ["REQUEST_METHOD"]
+        request["method"] = method
+
+        if method == "POST":
+            data = PostRequests().get_request_params(environ)
+            request["data"] = MyFramework.decode_value(data)
+            print(f"POST-request: {MyFramework.decode_value(data)}")
+        if method == "GET":
+            request_params = GetRequests().get_request_params(environ)
+            request["request_params"] = MyFramework.decode_value(request_params)
+            print(
+                f"GET-request parameters" f" {MyFramework.decode_value(request_params)}"
+            )
 
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound()
-        request = {}
 
         for front in self.fronts_lst:
             front(request)
         code, body = view(request)
-        start_response(code, [('Content-Type', 'text/html')])
-        return [body.encode('utf-8')]
+        start_response(code, [("Content-Type", "text/html")])
+        return [body.encode("utf-8")]
 
-
-class PageNotFound:
-    def __call__(self, request):
-        return '404 PAGE Not Found'
+    @staticmethod
+    def decode_value(data) -> dict:
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace("%", "=").replace("+", " "), "UTF-8")
+            val_decode_str = decodestring(val).decode("UTF-8")
+            new_data[k] = val_decode_str
+        return new_data
